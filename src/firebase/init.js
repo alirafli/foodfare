2f7f7c3991ca9aa8cc9c50f9ff3aefb606c04e53
Novel Bafagih <novel.bafagih@udana.id>
2022-03-16 07:46:50 +0700

    [ADD] login, register, logout

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -1,8 +1,9 @@
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
-import { firebaseConfig } from "./env";
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import { getAuth } from 'firebase/auth';
+import { firebaseConfig } from './env';
 
 export const firebaseApp = initializeApp(firebaseConfig);
-export const analytics = getAnalytics(firebaseApp);
-export const auth = getAuth(firebaseApp);
+
+export const analytics = getAnalytics(firebaseApp());
+export const auth = getAuth(firebaseApp());
