158c16e08e7fe154fcac0e835888b0c6e9d4fdbb
Novel Bafagih <novel.bafagih@udana.id>
2022-03-18 16:23:52 +0700

    [FIX] remove env.js

--- src/firebase/env.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyD6dftJ7W6ntQ8UmgVcEJIhg2yG9RUgkek",
-  authDomain: "foodfare-37619.firebaseapp.com",
-  projectId: "foodfare-37619",
-  storageBucket: "foodfare-37619.appspot.com",
-  messagingSenderId: "604720457585",
-  appId: "1:604720457585:web:750fc79864e1f0543978d0",
-  measurementId: "G-7XQJG2E1VS"
-};
