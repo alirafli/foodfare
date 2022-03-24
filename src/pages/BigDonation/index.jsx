785f5577f63b5e0e737c4406d6b679c16ebf29b9
Novel Bafagih <novel.bafagih@udana.id>
2022-03-24 07:47:38 +0700

    Merge branch 'master' of https://github.com/alirafli/foodfare into novel_feature_data-model

--- src/pages/BigDonation/index.jsx
+++ src/pages/BigDonation/index.jsx
@@ -1,7 +1,7 @@
 import { Box, Grid, Typography } from '@mui/material';
 import React from 'react';
-import donatePhone from '../../assets/bigDonation/donate-phone.svg';
 import { useStyles } from './style';
+import TitleSection from './TitleSection';
 
 const BigDonation = () => {
   const classes = useStyles();
@@ -11,19 +11,7 @@ const BigDonation = () => {
         padding: '20px',
       }}
     >
-      <Grid container spacing={3} alignItems="center" justifyContent="center">
-        <Grid item md={7} order={{ xs: 2, md: 1 }}>
-          <Typography variant="h1" color="text.primary">
-            In this <Box className={classes.boldText}>March</Box>,
-          </Typography>
-          <Typography variant="h1" color="text.primary">
-            Let's donate food to the <Box className={classes.boldText}>orphanage</Box>!
-          </Typography>
-        </Grid>
-        <Grid item md={5} order={{ xs: 1, md: 2 }}>
-          <img src={donatePhone} alt="" srcset="" />
-        </Grid>
-      </Grid>
+      <TitleSection classes={classes}/>
     </Box>
   );
 };
