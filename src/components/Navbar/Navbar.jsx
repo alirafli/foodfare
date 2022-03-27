a2e1854c77a2bea2b9417cb6639af45dd016bc17
alirafli <mrafli1212@gmail.com>
2022-03-27 14:35:46 +0700

    [ADD] navbar to route

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+const pagesUser = ["Share", "Donate", "Article", "About Us"];
+const settings = ["Profile", "Account", "Dashboard", "Logout"];
+
+const Navbar = () => {
+  // const [user, loading, error] = useAuthState(auth);
+  // console.log(user, loading, error)
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="fixed">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
+
+export default Navbar;
